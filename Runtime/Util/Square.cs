62107350a594c34bc597146401d83ccdeab452ab
Anatta336 <sdriver@gmx.co.uk>
2020-06-06 23:52:51 +0100

    remove exception

--- Runtime/Util/Square.cs
+++ Runtime/Util/Square.cs
@@ -3,36 +3,36 @@ using System.Linq;
 
 namespace SamDriver.Decal
 {
-  /// <summary>
-  /// Provides convenient access to positions of corners of a square.
-  /// </summary>
-  internal static class Square
-  {
-    public const int CornerCount = 4;
-
-    static Float2[] _centeredUnitSquareCorners;
-    public static Float2[] CenteredUnitSquareCorners
+    /// <summary>
+    /// Provides convenient access to positions of corners of a square.
+    /// </summary>
+    internal static class Square
     {
-      get
-      {
-        if (_centeredUnitSquareCorners == null || _centeredUnitSquareCorners.Length == 0)
+        public const int CornerCount = 4;
+
+        static Float2[] _centeredUnitSquareCorners;
+        public static Float2[] CenteredUnitSquareCorners
         {
-          _centeredUnitSquareCorners = SquareCorners(0f, 0f, 1f).ToArray();
-          if (_centeredUnitSquareCorners.Length != CornerCount)
-          {
-            throw new DecalException($"{nameof(SquareCorners)} expected to give {CornerCount} values, but gave {_centeredUnitSquareCorners.Length}.");
-          }
+            get
+            {
+                if (_centeredUnitSquareCorners == null || _centeredUnitSquareCorners.Length == 0)
+                {
+                    _centeredUnitSquareCorners = SquareCorners(0f, 0f, 1f).ToArray();
+                    if (_centeredUnitSquareCorners.Length != CornerCount)
+                    {
+                        throw new System.Exception($"{nameof(SquareCorners)} expected to give {CornerCount} values, but gave {_centeredUnitSquareCorners.Length}.");
+                    }
+                }
+                return _centeredUnitSquareCorners;
+            }
         }
-        return _centeredUnitSquareCorners;
-      }
-    }
 
-    public static IEnumerable<Float2> SquareCorners(float midX = 0f, float midY = 0f, float size = 1f)
-    {
-      yield return new Float2(midX - size * 0.5f, midY - size * 0.5f);
-      yield return new Float2(midX + size * 0.5f, midY - size * 0.5f);
-      yield return new Float2(midX + size * 0.5f, midY + size * 0.5f);
-      yield return new Float2(midX - size * 0.5f, midY + size * 0.5f);
+        public static IEnumerable<Float2> SquareCorners(float midX = 0f, float midY = 0f, float size = 1f)
+        {
+            yield return new Float2(midX - size * 0.5f, midY - size * 0.5f);
+            yield return new Float2(midX + size * 0.5f, midY - size * 0.5f);
+            yield return new Float2(midX + size * 0.5f, midY + size * 0.5f);
+            yield return new Float2(midX - size * 0.5f, midY + size * 0.5f);
+        }
     }
-  }
 }
