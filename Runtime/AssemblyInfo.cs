e358ead89238a8a504d232dcbd34bcc8c8ed5b31
Anatta336 <sdriver@gmx.co.uk>
2020-06-08 17:21:53 +0100

    tests for Triangle

---
+++ Runtime/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+// make all internals in this assembly also visible to the test assembly(s)
+[assembly: InternalsVisibleTo("SamDriver.DrivenDecals.EditorTests")]
